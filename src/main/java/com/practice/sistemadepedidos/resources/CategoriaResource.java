00fe1b343a3c5300a8dba9c16cb85c916ec6001e
thiago12prata <thiago12prata@gmail.com>
2021-11-22 12:11:13 -0300

    Entidade Categoria

--- src/main/java/com/practice/sistemadepedidos/resources/CategoriaResource.java
+++ src/main/java/com/practice/sistemadepedidos/resources/CategoriaResource.java
@@ -1,8 +1,14 @@
 package com.practice.sistemadepedidos.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.practice.sistemadepedidos.entities.Categoria;
 
 
 @RestController
@@ -10,7 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1L, "informatica");
+		Categoria cat2 = new Categoria(2L, "Escritorio");
+		List<Categoria> list = new ArrayList<>();
+		list.addAll(Arrays.asList(cat1,cat2));
+		
+		return list;
 	}
 }
