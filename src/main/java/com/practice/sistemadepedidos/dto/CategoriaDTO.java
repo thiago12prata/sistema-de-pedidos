6f2d7fccd855f87ade3d4fd1a34ca6faf57722e3
thiago12prata <thiago12prata@gmail.com>
2021-11-25 16:12:13 -0300

    Validação sintatica com bean validation

--- src/main/java/com/practice/sistemadepedidos/dto/CategoriaDTO.java
+++ src/main/java/com/practice/sistemadepedidos/dto/CategoriaDTO.java
@@ -2,12 +2,18 @@ package com.practice.sistemadepedidos.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.practice.sistemadepedidos.entities.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	@NotEmpty(message="Preenchimento obrigatorio")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
@@ -32,5 +38,9 @@ public class CategoriaDTO implements Serializable{
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public static Categoria toEntity(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(),objDTO.getNome());
 	}
 }
--- src/main/java/com/practice/sistemadepedidos/resources/CategoriaResource.java
+++ src/main/java/com/practice/sistemadepedidos/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -35,13 +37,15 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(service.findById(id));
 	}
 	@PostMapping
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO){
+		Categoria obj = CategoriaDTO.toEntity(objDTO);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Long id){
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDTO, @PathVariable Long id){
+		Categoria obj = CategoriaDTO.toEntity(objDTO);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
