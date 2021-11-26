20447d0c23af2483ff1a2a8e0658aebd9c2042f7
thiago12prata <thiago12prata@gmail.com>
2021-11-26 10:37:32 -0300

    Put delete e get para entidade cliente

--- src/main/java/com/practice/sistemadepedidos/resources/ClienteResource.java
+++ src/main/java/com/practice/sistemadepedidos/resources/ClienteResource.java
@@ -1,12 +1,26 @@
 package com.practice.sistemadepedidos.resources;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.practice.sistemadepedidos.dto.ClienteDTO;
 import com.practice.sistemadepedidos.entities.Cliente;
 import com.practice.sistemadepedidos.services.ClienteService;
 
@@ -19,7 +33,45 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Cliente> finById(@PathVariable Long id) {
-		return ResponseEntity.ok().body(service.finById(id));
+	public ResponseEntity<Cliente> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDTO){
+		Cliente obj = ClienteDTO.toEntity(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Long id){
+		Cliente obj = ClienteDTO.toEntity(objDTO);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Cliente> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<ClienteDTO> listDTO =  new ArrayList<>();
+		List<Cliente> list =  service.findAll();	
+		for(Cliente c: list) {
+			listDTO.add(new ClienteDTO(c));
+		}
+		return ResponseEntity.ok().body(listDTO);
+	}
+	@GetMapping(value = "/paged")
+	public ResponseEntity<Page<ClienteDTO>> findAllPaged(
+			@RequestParam(value = "pages", defaultValue = "0") Integer pages, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Cliente> page =  service.findAllPaged(pages, linesPerPage, orderBy, direction);	
+		Page<ClienteDTO> pageDTO = page.map(c -> new ClienteDTO(c));
+		return ResponseEntity.ok().body(pageDTO);
 	}
 }
--- src/main/java/com/practice/sistemadepedidos/services/ClienteService.java
+++ src/main/java/com/practice/sistemadepedidos/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.practice.sistemadepedidos.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.practice.sistemadepedidos.entities.Cliente;
 import com.practice.sistemadepedidos.repositories.ClienteRepository;
+import com.practice.sistemadepedidos.servicesexception.DataIntegrityException;
+import com.practice.sistemadepedidos.servicesexception.ResourceNotFoundException;
 
 @Service
 public class ClienteService {
@@ -14,8 +21,42 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repository;
 	
-	public Cliente finById(Long id) {
+	public Cliente findById(Long id) {
 		Optional<Cliente> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ResourceNotFoundException("Não foi encontrato o recurso: " 
+			+ Cliente.class.getName()
+			+" com a id "
+			+ id 
+		));
+	}
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+	public Cliente update(Cliente obj) {
+		Cliente newObj =findById(obj.getId());
+		atualizarDados(newObj, obj);
+		return repository.save(newObj);
+	}
+	public void delete(Long id) {
+		repository.findById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
+	}
+	public List<Cliente> findAll() {
+		return repository.findAll();
+	}
+	public Page<Cliente> findAllPaged(Integer pages, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(pages, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repository.findAll(pageRequest);
+	}
+	
+	private void atualizarDados(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());		
 	}
 }
