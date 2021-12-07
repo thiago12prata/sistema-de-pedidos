12cf2d12112e7704d85b768865b88b4858b54200
thiago12prata <thiago12prata@gmail.com>
2021-12-07 13:03:45 -0300

    Enviando email com estilo HTML

--- src/main/java/com/practice/sistemadepedidos/config/JacksonConfig.java
+++ src/main/java/com/practice/sistemadepedidos/config/JacksonConfig.java
@@ -3,6 +3,8 @@ package com.practice.sistemadepedidos.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.sistemadepedidos.entities.PagamentoComBoleto;
@@ -20,5 +22,9 @@ public class JacksonConfig {
 			}
 		};
 		return builder;
+	}
+	@Bean
+	public JavaMailSender jMS (){
+		return new JavaMailSenderImpl();
 	}
 }
