783281d44beb383fbbe04b04bd4123c1af452356
thiago12prata <thiago12prata@gmail.com>
2021-12-11 22:44:39 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/practice/sistemadepedidos/SistemaDePedidosApplication.java
+++ src/main/java/com/practice/sistemadepedidos/SistemaDePedidosApplication.java
@@ -1,13 +1,26 @@
 package com.practice.sistemadepedidos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SistemaDePedidosApplication {
+import com.practice.sistemadepedidos.services.S3Service;
 
+@SpringBootApplication
+public class SistemaDePedidosApplication implements CommandLineRunner{
+
+	
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaDePedidosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\d.jpg");
 	}
 
 }
